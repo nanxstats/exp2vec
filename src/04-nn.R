964c5f09091f227f001e1cbed0d5af1d8581cf7a
Nan Xiao <me@nanx.me>
2022-03-25 22:29:20 -0400

    Use a more flexible base solution

--- src/04-nn.R
+++ src/04-nn.R
@@ -1,39 +1,26 @@
 tissue_name <- "Pancreas"
 word_vectors <- readRDS(file.path("output", paste0(tissue_name, "_embedding.rds")))
 
-interest <- tibble::tibble(term = c("EGFR", "TP53", "PTEN", "KRAS"))
+interest <- c("EGFR", "TP53", "PTEN", "KRAS")
 
 # Use the embedding to find nearest-neighbor genes
-result <- dplyr::mutate(
-  interest,
-  # Create similarity matrix
-  cos_sim = purrr::map(
-    term,
-    function(x) {
-      text2vec::sim2(word_vectors, y = word_vectors[x, , drop = FALSE], method = "cosine", norm = "l2")
-    }
-  ),
-  # Convert matrix to tibble
-  cos_sim_tbl = purrr::map(
-    cos_sim,
-    function(x) {
-      tibble::tibble(term = row.names(x), distance = x[, 1]) |>
-        dplyr::arrange(desc(distance))
-    }
-  ), # Sort distance
-  # Find neighbors
-  n1 = purrr::map_chr(cos_sim_tbl, function(x) x[[2, 1]]),
-  n2 = purrr::map_chr(cos_sim_tbl, function(x) x[[3, 1]]),
-  n3 = purrr::map_chr(cos_sim_tbl, function(x) x[[4, 1]]),
-  n4 = purrr::map_chr(cos_sim_tbl, function(x) x[[5, 1]]),
-  n5 = purrr::map_chr(cos_sim_tbl, function(x) x[[6, 1]])
-)
+query_gene <- function(gene, word_vectors, method = "cosine", norm = "l2") {
+  vec <- text2vec::sim2(word_vectors, y = word_vectors[gene, , drop = FALSE], method = method, norm = norm)
+  df <- data.frame(gene = rownames(vec), sim = vec[, 1], stringsAsFactors = FALSE)
+  df <- df[!(df$gene == gene), ]
+  df <- df[order(df$sim, decreasing = TRUE), ]
+  rownames(df) <- NULL
+  df
+}
+
+lst <- lapply(interest, FUN = query_gene, word_vectors = word_vectors)
+df <- do.call(cbind, args = lst)
+names(df) <- as.vector(t(outer(interest, c("gene", "sim"), paste, sep = ".")))
 
 # Print table
-result |>
-  dplyr::select(-starts_with("cos")) |>
-  kableExtra::kbl(escape = FALSE) |>
-  kableExtra::kable_classic(full_width = FALSE)
+df |>
+  DT::datatable() |>
+  DT::formatRound(columns = seq_len(length(interest)) * 2, digits = 4)
 
 # EGFR: CCT6A <https://doi.org/10.1002/pmic.201800157>
 # PTEN: EIF4EBP2 <https://doi.org/10.1158/1541-7786.mcr-17-0696>
